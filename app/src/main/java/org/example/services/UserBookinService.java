126f3ca84f19ab2fbcf3aec6ceeae27dd8e79c4b
kartikrathi089 <kartikrathi089@gmail.com>
2026-06-08 13:33:09 +0530

    entities

---
+++ app/src/main/java/org/example/services/UserBookinService.java
@@ -0,0 +1,11 @@
+package org.example.services;
+
+import org.example.entities.User;
+
+public class UserBookinService {
+    private User user;
+    public UserBookinService(User user){
+        this.user=user;
+
+    }
+}
